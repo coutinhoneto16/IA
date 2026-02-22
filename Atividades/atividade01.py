6850acfbd14ea872fd3a5ec539901da615c45890
Neto Coutinho <coutinhoneto16@gmail.com>
2026-02-22 15:32:25 -0300

    Fazendo a IA aprender AND e OR

---
+++ Atividades/atividade01.py
@@ -0,0 +1,59 @@
+import random
+class Neuronio:
+
+    def __init__(self, n_parametros, eta = 0.05):
+        self.eta = eta
+        self.w = [random.uniform(-1,1) for _ in range(n_parametros)]
+        self.b = random.uniform(-1,1)
+
+    def ativacao(self, soma):
+        if soma >= 0.0:
+            return 1
+        else:
+            return 0
+
+    def teste(self, x):
+        soma = sum(w_i * x_i for w_i, x_i in zip(self.w, x)) + self.b
+        return self.ativacao(soma)
+
+
+    def treinamento(self, dados, n_loops = 100):
+        for _ in range(n_loops):
+            for x, esperado in dados:
+                y = self.teste(x)
+                erro = esperado - y
+
+                for i in range(len(self.w)):
+                    self.w[i] += self.eta * erro * x[i]
+
+                self.b += self.eta * erro
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    dados_and = [
+        ([0,0], 0),
+        ([1,0], 0),
+        ([0,1], 0),
+        ([1,1], 1),
+    ]
+
+    dados_or = [
+        ([0,0], 0),
+        ([1,0], 1),
+        ([0,1], 1),
+        ([1,1], 1),
+    ]
+
+    print("Treinando AND:")
+    n_and = Neuronio(2)
+    n_and.treinamento(dados_and, n_loops=100)
+    for x, _ in dados_and:
+        print(x, " -> ", n_and.teste(x))
+
+
+    print("\nTreinando OR:")
+    n_or = Neuronio(2)
+    n_or.treinamento(dados_or, n_loops=100)
+    for x, _ in dados_or:
+        print(x, " -> ", n_or.teste(x))
